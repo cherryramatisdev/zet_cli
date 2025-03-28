35afa86ff777e5bff44eebc27fc7c683eb5bd816
Cherry Ramatis <cherry.ramatis@icloud.com>
2025-03-28 18:56:21 -0300

    Roughly match commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+use cli::Cli;
+
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        cli::Commands::Init => todo!(),
+        cli::Commands::Titles => todo!(),
+        cli::Commands::Grep { term } => todo!(),
+        cli::Commands::Create => todo!(),
+        cli::Commands::Config { command } => todo!(),
+        cli::Commands::Now => todo!(),
+        cli::Commands::Agenda => todo!(),
+        cli::Commands::Import { path } => todo!(),
+        _ => {
+            // TODO: this will be the <query> part of the command
+            todo!()
+        }
+    }
 }

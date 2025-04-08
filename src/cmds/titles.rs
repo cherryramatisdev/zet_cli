4af44c0b789d0a04b8d7bac507a4d1bf739e653d
Cherry Ramatis <cherry.ramatis@icloud.com>
2025-04-08 14:20:07 -0300

    Fix titles command to properly use schema_path

--- src/cmds/titles.rs
+++ src/cmds/titles.rs
@@ -1,15 +1,24 @@
 use anyhow::Result;
-use regex::Regex;
 use colored::Colorize;
+use regex::Regex;
 
 use crate::repo_schema::RepoSchema;
 
 pub fn call() -> Result<()> {
+    let schema_path = RepoSchema::get_schema_path()?;
     let schema = RepoSchema::get_config()?;
 
     for entry in schema.entries {
-        let tags = extract_tags(format!("{}/{}", entry.dir_path, entry.entry_file))?;
-        eprintln!("{} - {} {}", entry.id, entry.title, tags.join(" ").bright_black().bold());
+        let tags = extract_tags(format!(
+            "{}/{}/{}",
+            schema_path, entry.dir_path, entry.entry_file
+        ))?;
+        eprintln!(
+            "{} - {} {}",
+            entry.id,
+            entry.title,
+            tags.join(" ").bright_black().bold()
+        );
     }
 
     Ok(())
@@ -20,7 +29,8 @@ fn extract_tags(file_path: String) -> Result<Vec<String>> {
     let re = Regex::new(r"#\w+").unwrap(); // `\w` = letters, numbers, underscore
 
     // Search entire file
-    let tags: Vec<String> = re.find_iter(&content)
+    let tags: Vec<String> = re
+        .find_iter(&content)
         .map(|m| m.as_str().to_string())
         .collect();
 

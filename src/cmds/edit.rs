529f846b99c63e2ccf15602cb642e3bff7a9023c
Cherry Ramatis <cherry.ramatis@icloud.com>
2025-04-08 14:20:24 -0300

    Make edit command register a new git commit

--- src/cmds/edit.rs
+++ src/cmds/edit.rs
@@ -1,6 +1,9 @@
 use anyhow::{bail, Result};
 
-use crate::repo_schema::{find_entry_by_term, RepoSchema};
+use crate::{
+    git_utils,
+    repo_schema::{find_entry_by_term, RepoSchema},
+};
 
 pub fn call(term: String) -> Result<()> {
     let schema_path = RepoSchema::get_schema_path()?;
@@ -10,16 +13,14 @@ pub fn call(term: String) -> Result<()> {
 
     match entry {
         Some(entry) => {
-            std::process::Command::new("nvim")
-                .arg(format!(
-                    "{}/{}/{}",
-                    schema_path, entry.dir_path, entry.entry_file
-                ))
-                .status()?;
+            let path = format!("{}/{}/{}", schema_path, entry.dir_path, entry.entry_file);
+            std::process::Command::new("nvim").arg(&path).status()?;
 
             entry.modified_at = Some(chrono::Local::now().naive_local());
 
             schema.save()?;
+
+            git_utils::commit(format!("Changed note: {}", &path), Some(schema_path))?;
 
             Ok(())
         }
